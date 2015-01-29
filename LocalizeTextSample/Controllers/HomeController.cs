739e4effa9b37e931fd59dc31ebd62f2c2986e6e
jsakamoto <junichi.jp@gmail.com>
2015-01-29 09:41:56 +0900

    Initial commit, not localized.

---
+++ LocalizeTextSample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LocalizeTextSample.Models;
+
+namespace LocalizeTextSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUserProfile(UserProfile userProfile)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View("Index", userProfile);
+            }
+            return View(userProfile);
+        }
+    }
+}
---
+++ LocalizeTextSample/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LocalizeTextSample.Models
+{
+    public class UserProfile
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
